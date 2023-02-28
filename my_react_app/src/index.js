9187b731c66a920f02b64aab9a1275e5f5c154c1
souravsarker2015 <souravsarker2015@gmail.com>
2023-03-01 02:34:12 +0600

    code added

--- my_react_app/src/index.js
+++ my_react_app/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => (
+    <React.Fragment>
+        <h1>hello</h1>
+        <h1>hello</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </React.Fragment>
+)
+ReactDom.render(<App/>, document.getElementById('root'))
+
+// const App = () => (
+//     <div className={'first_class'}>
+//         <h1>first</h1>
+//         <h1>Second</h1>
+//     </div>
+// )
+// ReactDom.render(<App/>, document.getElementById('root'))
+
+
+// const App = () => {
+//     // return React.createElement('h1',{className:'h1_class'},'hello react')
+//     return React.createElement('h1',{className:'h1_class'},React.createElement('div',{className:'in_div'},'inside h1 tag and it is a div'))
+// }
+// ReactDom.render(<App/>, document.getElementById('root'))
+
+
+// import React from 'react'
+// import ReactDom from 'react-dom'
+//
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>Hello React</h1>
+//         </div>
+//     )
+// }
+// ReactDom.render(<App/>, document.getElementById('root'))
